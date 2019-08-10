d30e45e293992958eb430dfa0c4127cb9feaaffb
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2019-08-10 17:14:24 +0100

    Replace TV show names with images

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React from 'react';
-import piratesimage from './pirates-of-the-caribbean.jpg';
+import goodlifeimage from './images/goodlife.jpg'
+import bottomimage from './images/bottom.jpg'
+import fawltytowersimage from './images/fawltytowers.jpg'
+import onlyfoolsimage from './images/onlyfools.jpg'
+import yesministerimage from './images/yesprimeminister.jpg'
+import blackadderimage from './images/blackadder.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <img src={piratesimage} alt="pirates film" />
-        <br /><br /><br />
-        <div className='container'>
-          <div>The Good Life</div>
-          <div>Only Fools and Horses</div>
-          <div>Blackadder</div>
+      <div className='container'>
+        <div>
+          <img src={goodlifeimage} alt="good life" />
         </div>
-        <div className='container'>
-          <div>Bottom</div>
-          <div>Faulty Towers</div>
-          <div>Yes Minister</div>
+        <div>
+          <img src={bottomimage} alt="bottom" />
         </div>
+        <div>
+          <img src={fawltytowersimage} alt="fawlty towers" />
+        </div>
+      </div>
+      <div className='container'>
+        <div>
+          <img src={yesministerimage} alt="yes prime minister" />
+        </div>
+        <div>
+          <img src={onlyfoolsimage} alt="only fools" />
+        </div>
+        <div>
+          <img src={blackadderimage} alt="good life" />
+        </div>
+      </div>
     </div>
   );
 }
