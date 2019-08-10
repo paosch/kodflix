b314f5c7486ded4352f3e196c9b07e2e422de7be
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2019-08-10 13:31:43 +0100

    Add TV show names displayed in two rows

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ function App() {
   return (
     <div className="App">
         <img src={piratesimage} alt="pirates film" />
+        <br /><br /><br />
+        <div className='container'>
+          <div>The Good Life</div>
+          <div>Only Fools and Horses</div>
+          <div>Blackadder</div>
+        </div>
+        <div className='container'>
+          <div>Bottom</div>
+          <div>Faulty Towers</div>
+          <div>Yes Minister</div>
+        </div>
     </div>
   );
 }
