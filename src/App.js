56784a91ce5da45a7112b20297c0e0f74ad25051
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2019-08-24 09:31:20 +0100

    Add show title overlay on hover-over

--- src/App.js
+++ src/App.js
@@ -11,25 +11,43 @@ function App() {
   return (
     <div className="App">
       <div className='container'>
-        <div>
+        <div className='item'>
           <img src={goodlifeimage} alt="good life" />
+          <div className="overlay">
+            <h1>The Good Life</h1>
+          </div>
         </div>
-        <div>
+        <div className='item'>
           <img src={bottomimage} alt="bottom" />
+          <div className="overlay">
+            <h1>Bottom</h1>
+          </div>
         </div>
-        <div>
+        <div className='item'>
           <img src={fawltytowersimage} alt="fawlty towers" />
+          <div className="overlay">
+            <h1>Fawlty Towers</h1>
+          </div>
         </div>
       </div>
       <div className='container'>
-        <div>
+        <div className='item'>
           <img src={yesministerimage} alt="yes prime minister" />
+          <div className="overlay">
+            <h1>Yes Minister</h1>
+          </div>
         </div>
-        <div>
+        <div className='item'>
           <img src={onlyfoolsimage} alt="only fools" />
+          <div className="overlay">
+            <h1>Only Fools and Horses</h1>
+          </div>
         </div>
-        <div>
+        <div className='item'>
           <img src={blackadderimage} alt="good life" />
+          <div className="overlay">
+            <h1>Blackadder</h1>
+          </div>
         </div>
       </div>
     </div>
