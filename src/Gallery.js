9d98d8d3644d84e99c42af3bf1f2a3ce13c0a32d
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2019-08-24 16:27:20 +0100

    Unique details page for each comedy show

--- src/Gallery.js
+++ src/Gallery.js
@@ -11,14 +11,14 @@ export default function Gallery(){
   return (
     <div>
       <div className='container'>
-        <Show name='The Good life' image={goodlifeimage}/>
-        <Show name='Bottom' image={bottomimage}/>
-        <Show name='Fawlty Towers' image={fawltytowersimage}/>
+        <Show id='goodlife' name='The Good life' image={goodlifeimage}/>
+        <Show id='bottom' name='Bottom' image={bottomimage}/>
+        <Show id='fawltytowers' name='Fawlty Towers' image={fawltytowersimage}/>
       </div>
       <div className='container'>
-        <Show name='Yes, Minister' image={yesministerimage}/>
-        <Show name='Only Fools and Horses' image={onlyfoolsimage}/>
-        <Show name='Blackadder' image={blackadderimage}/>
+        <Show id='yesminister' name='Yes, Minister' image={yesministerimage}/>
+        <Show id='onlyfools' name='Only Fools and Horses' image={onlyfoolsimage}/>
+        <Show id='blackadder' name='Blackadder' image={blackadderimage}/>
       </div>
     </div>
   );
