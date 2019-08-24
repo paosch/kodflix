0b8c6f512dd575760dfd8969317b556b7812253b
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2019-08-24 21:36:01 +0100

    Make Details component stateful

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Details(){
-  return(
-    <div>
-      <h1>Welcome to the details page</h1>
-      <Link to='/'>Back to home page</Link>
-    </div>
-  )
+
+export default class Details extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      welcomeMessage: 'Hi, welcome to the details page'
+    };
+  }
+
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({
+        welcomeMessage: 'The best is yet to come!'
+      });
+    }, 3000);
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>{this.state.welcomeMessage}</h1>
+        <Link to='/'>Back to home page</Link>
+      </div>
+    )
+  }
 }
