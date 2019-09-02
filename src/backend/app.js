dc2b9c05cad8385b0432333627ab5417f080d2c3
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2019-09-02 22:14:58 +0100

    Return a list of shows from the backend

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const data = require('./shows');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/rest/shows', (req, res) => res.send(data));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
