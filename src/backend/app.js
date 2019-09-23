b2643d6696cd08e5a33afd7d54c5505c52c74c13
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2019-09-23 18:08:07 +0100

    Make changes to enable the backend server in prod

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,15 +1,15 @@
+const path = require('path')
 const express = require('express');
 const app = express();
-// const port = 3001;
 const port = process.env.PORT || 3001;
 const getShows = require('./shows');
 
 app.get('/rest/shows', (req, res) => res.send(getShows()));
 
-app.use(express.static(path.join(__dirname, '/Users/paola/Documents/JSpractising/kodflix/build')));
+app.use(express.static(path.join(__dirname, '../../build')));
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '/Users/paola/Documents/JSpractising/kodflix/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
